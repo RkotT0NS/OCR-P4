eb8ed8bd6684eef99f6bf829b1341703f1491587
Fabien Jacq <tetedacier@proton.me>
2026-01-23 12:23:04 +0100

    fix(font): Add missing font support for stories

--- figma/implementation/.storybook/main.ts
+++ figma/implementation/.storybook/main.ts
@@ -1,6 +1,23 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
+function generateFontUrl(name: string, size: number[]): string {
+    // return `family=${name}:wght@0,100;0,300;0,700;0,900`; //&amp;family=Inter:wght@0,100..900;1,100..900`; //${size.join(';')}`;
+    return `family=${name}:ital,wght@0,100..900;1,100..900&subset=latin`;
+    //&amp;family=Inter:wght@0,100..900;1,100..900`; //${size.join(';')}`;
+    // return `family=${name}:wght@1,100..900`; //${size.join(';')}`;
+}
 const config: StorybookConfig = {
+  previewBody: (body) => `
+    ${body}
+    <link
+        href="https://fonts.googleapis.com/css?${generateFontUrl('DM Sans', [100, 300, 700])}"
+        rel="stylesheet"
+    />
+    <link
+        href="https://fonts.googleapis.com/css?${generateFontUrl('Inter', [100, 300, 700])}"
+        rel="stylesheet"
+    />
+  `,
   "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
