56241bd1621614a068ef7f87c04fd6897bca790c
Fabien Jacq <tetedacier@proton.me>
2026-02-02 15:16:55 +0100

    refac(readabilty): Make HomePage component properties definition more visible

--- laravel/resources/js/pages/welcome.tsx
+++ laravel/resources/js/pages/welcome.tsx
@@ -1,10 +1,10 @@
 import { dashboard, login, register } from '@/routes';
 import { type SharedData } from '@/types';
-import { HomePage, Icons } from '../../../../figma/implementation/src';
 import { Link, usePage } from '@inertiajs/react';
-import { useContext } from 'react';
 import Uppy from '@uppy/core';
 import Tus from '@uppy/tus';
+import { useContext } from 'react';
+import { HomePage, Icons } from '../../../../figma/implementation/src';
 
 export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
@@ -22,27 +22,30 @@ export default function Welcome() {
                 lockIcon: '/ui/lockIcon.png',
             }}
         >
-            <HomePage user={auth.user} uploader={async (file: File) => {
-                if (file) {
-                    console.log("Uploader le fichier", file);
-                    const uppy = new Uppy();
-                    uppy.addFile(file);
+            <HomePage
+                user={auth.user}
+                uploader={async (file: File) => {
+                    if (file) {
+                        console.log('Uploader le fichier', file);
+                        const uppy = new Uppy();
+                        uppy.addFile(file);
 
-                    uppy.use(Tus, {
-                        endpoint: '/api/upload', // Points to your Laravel route
-                        chunkSize: 500 * 1024, // 500KB chunks
-                        headers: {
-                            Authorization: `Bearer ${auth.token}`,
-                        },
-                    });
-                    uppy.on('complete', (result) => {
-                        console.log('Upload complete!', result);
-                    });
-                    uppy.upload().then((result) => {
-                        console.log('Upload complete!', result);
-                    });
-                }
-            }} />
+                        uppy.use(Tus, {
+                            endpoint: '/api/upload', // Points to your Laravel route
+                            chunkSize: 500 * 1024, // 500KB chunks
+                            headers: {
+                                Authorization: `Bearer ${auth.token}`,
+                            },
+                        });
+                        uppy.on('complete', (result) => {
+                            console.log('Upload complete!', result);
+                        });
+                        uppy.upload().then((result) => {
+                            console.log('Upload complete!', result);
+                        });
+                    }
+                }}
+            />
         </Icons.Provider>
     );
 }
