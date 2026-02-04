51412d1836ed48844d3bddcefae2b7c0e478c23c
Fabien Jacq <tetedacier@proton.me>
2026-02-04 17:06:13 +0100

    fix(download): Restore broken story

--- figma/implementation/src/pages/download-without-password.tsx
+++ figma/implementation/src/pages/download-without-password.tsx
@@ -2,9 +2,10 @@ import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import type { Upload } from "@datashare/types";
 import MimeTypeIcon from "../components/MimeTypeIcon";
-import { humanFileSize } from "../lib/size-format";
 import { formatDistance } from "date-fns";
 import { fr } from "date-fns/locale";
+import { cn } from "../lib/utils";
+import { humanFileSize } from "../lib/size-format";
 import { Callout } from "../components/Callout";
 const alertIcon =
   "https://www.figma.com/api/mcp/asset/38a7ba09-95e2-4753-b5e3-b0b749adc97a";
@@ -13,13 +14,19 @@ const downloadIcon =
 
 function FileInfo({ fileDetails }: { fileDetails: Upload }) {
   return (
-    <div className="flex items-center w-full gap-4 p-2">
+    <div className={cn("flex items-center w-full gap-4 p-2")}>
       <MimeTypeIcon mimeType={fileDetails.mime_type} classes="w-6 h-6" />
-      <div className="flex-1">
-        <p className="text-base text-ellipsis overflow-hidden whitespace-nowrap font-medium">
+      <div className={cn("flex-1")}>
+        <p
+          className={cn(
+            "text-base text-ellipsis overflow-hidden whitespace-nowrap font-medium text-ellipsis ",
+          )}
+        >
           {fileDetails.original_name}
         </p>
-        <p className="text-sm text-black">{humanFileSize(fileDetails.size)}</p>
+        <p className={cn("text-sm text-black")}>
+          {humanFileSize(fileDetails.size)}
+        </p>
       </div>
     </div>
   );
@@ -27,9 +34,13 @@ function FileInfo({ fileDetails }: { fileDetails: Upload }) {
 
 export function LocalCallout({ label }: { label: string }) {
   return (
-    <div className="bg-[#fff5ed] border border-[#e6cbb5] rounded-lg flex items-center gap-2 p-2 w-full">
-      <img src={alertIcon} alt="alert icon" className="w-4 h-4" />
-      <p className="text-[#aa642b] text-sm">{label}</p>
+    <div
+      className={cn(
+        "bg-[#fff5ed] border border-[#e6cbb5] rounded-lg flex items-center gap-2 p-2 w-full",
+      )}
+    >
+      <img src={alertIcon} alt="alert icon" className={cn("w-4 h-4")} />
+      <p className={cn("text-[#aa642b] text-sm")}>{label}</p>
     </div>
   );
 }
@@ -37,22 +48,30 @@ export function LocalCallout({ label }: { label: string }) {
 function DownloadWithoutPasswordPage({ fileDetails }: { fileDetails: Upload }) {
   return (
     <div
-      className="relative w-full h-screen"
+      className={cn("relative w-full h-screen")}
       style={{
         backgroundImage:
           "linear-gradient(174.9deg, #FFB88C 2.29%, #DE6262 97.71%)",
       }}
     >
       <Header />
-      <main className="w-full h-full flex flex-col items-center justify-center">
-        <div className="bg-white flex flex-col gap-6 items-center max-w-xl w-full p-8 rounded-2xl shadow-lg">
-          <h2 className="text-3xl font-bold text-black text-center">
+      <main
+        className={cn(
+          "w-full h-full flex flex-col items-center justify-center",
+        )}
+      >
+        <div
+          className={cn(
+            "bg-white flex flex-col gap-6 items-center max-w-xl w-full p-8 rounded-2xl shadow-lg",
+          )}
+        >
+          <h2 className={cn("text-3xl font-bold text-black text-center")}>
             Télécharger un fichier
           </h2>
-          <div className="flex flex-col gap-4 w-full">
+          <div className={cn("flex flex-col gap-4 w-full")}>
             <FileInfo {...{ fileDetails }} />
             <Callout
-              className="w-full"
+              className={cn("w-full")}
               type="Info"
               label={`Ce fichier expirera ${formatDistance(
                 new Date(fileDetails.expires_at),
@@ -66,9 +85,15 @@ function DownloadWithoutPasswordPage({ fileDetails }: { fileDetails: Upload }) {
           </div>
           <a
             href={fileDetails.download_url}
-            className="bg-[#ff812d]/13 border border-[#cd5e14]/50 text-[#ba681f] p-3 rounded-lg w-full flex items-center justify-center gap-2"
+            className={cn(
+              "bg-[#ff812d]/13 border border-[#cd5e14]/50 text-[#ba681f] p-3 rounded-lg w-full flex items-center justify-center gap-2",
+            )}
           >
-            <img src={downloadIcon} alt="download icon" className="w-4 h-4" />
+            <img
+              src={downloadIcon}
+              alt="download icon"
+              className={cn("w-4 h-4")}
+            />
             <span>Télécharger</span>
           </a>
         </div>
--- figma/implementation/src/stories/DownloadWithoutPasswordPage.stories.ts
+++ figma/implementation/src/stories/DownloadWithoutPasswordPage.stories.ts
@@ -1,16 +1,28 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
-import DownloadWithoutPasswordPage from '../pages/download-without-password';
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import DownloadWithoutPasswordPage from "../pages/download-without-password";
 
 const meta = {
-  title: 'Example/Page/Download',
+  title: "Example/Page/Download",
   component: DownloadWithoutPasswordPage,
-  tags: ['!autodocs'],
+  tags: ["!autodocs"],
   parameters: {
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 } satisfies Meta<typeof DownloadWithoutPasswordPage>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const DownloadWithoutPassword: Story = {};
+export const DownloadWithoutPassword: Story = {
+  args: {
+    fileDetails: {
+      id: "1234567890",
+      original_name:
+        "example_______________________________________________________________________________.pdf",
+      download_url: "about:blank",
+      expires_at: new Date(Date.now() + 3600000).toLocaleString(),
+      size: 1024,
+      type: "application/pdf",
+    },
+  },
+};
